67c6739724106f5195cf89ea670166454e81a0ec
Vig_345 <72251437+vign2020@users.noreply.github.com>
2023-06-25 11:45:00 +0530

    completeed

--- smart-project/webapp/views.py
+++ smart-project/webapp/views.py
@@ -5,4 +5,18 @@ views=Blueprint('views',__name__)
 
 @views.route('/')
 def home():
+    return render_template('home.html')
+@views.route('/crime')
+def crime():
     return render_template('index.html')
+
+@views.route('/arrest')
+def arrest():
+    return render_template('index2.html')
+@views.route('/story1')
+def story1(): 
+    return render_template('story1.html')
+
+@views.route('/story2')
+def story2(): 
+    return render_template('story2.html')
